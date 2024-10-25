4b524012ccebf4079a50d8299f67d7230b75a16f
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2024-10-25 17:24:06 -0500

    a

--- ProyectoTaller3/src/Ejercicio2.java
+++ ProyectoTaller3/src/Ejercicio2.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author HP
- */
 public class Ejercicio2 {
+    public static void main(String[] args) {
+           int v1, v2;
+           double resultado;
+           v1 = 10;
+           v2 = 10;
+           resultado = Math.pow(v1, 2) + v2;
+           System.out.println("resultado = " + resultado);
+           
+    }
     
 }

f21f45a3b9f4f8912bb692a7148582d305c42ff8
JuanDaa0304 <jdvargas17@utpl.edu.ec>
2024-10-25 18:54:46 -0500

    commit4

--- ProyectoTaller3/src/Ejercicio7.java
+++ ProyectoTaller3/src/Ejercicio7.java
@@ -1,12 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author HP
- */
 public class Ejercicio7 {
-    
+    public static void main(String[] args) {
+        double v1, v2, v3, v4, v5, v6, v7, v8;
+        boolean resultado;
+        v1 = Math.sqrt(81)+9;
+        v2 = 3;
+        v3 = 9;
+        v4 = 10;
+        v5 = 1;
+        v6 = 100;
+        v7 = 25;
+        v8 = Math.sqrt (100);
+        resultado = ((v1/v2) == v3) || (v4 > v5) && ((v6/v7)>=v8);
+        System.out.println("resultado = " + resultado);
+        //        
+    }
 }
--- ProyectoTaller3/src/Ejercicio9.java
+++ ProyectoTaller3/src/Ejercicio9.java
@@ -1,12 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author HP
- */
 public class Ejercicio9 {
-    
+    public static void main(String[] args) {
+        double v1, v2, v3, v4;
+        boolean resultado;
+        v1 = Math.sqrt(81);
+        v2 = Math.sqrt(25);
+        v3 = 10;
+        v4 = 50;
+        resultado = ((v1+v2)*v3) >= v4;
+        System.out.println("resultado = " + resultado);
+        
+    }
 }

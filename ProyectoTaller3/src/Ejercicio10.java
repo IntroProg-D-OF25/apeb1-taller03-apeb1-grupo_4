0e67af7f7774f676958a32ada13b64616370443c
JuanDaa0304 <jdvargas17@utpl.edu.ec>
2024-10-24 20:14:37 -0500

    Primer ejercicio

---
+++ ProyectoTaller3/src/Ejercicio10.java
@@ -0,0 +1,15 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int v1, v2, v3, v4, v5, v6;
+        double respt;
+        v1 = 1;
+        v2 = 2;
+        v3 = 4;
+        v4 = 3;
+        v5 = 5;
+        v6 = 81;
+        respt = ((v1+v2+v3+v4) / v5 ) + Math.sqrt(v6);
+        System.out.println("respt = " + respt);
+        
+    }
+}

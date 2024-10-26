efc91939d5b06c14a3dcabc61ee7da59136fe334
JuanDaa0304 <jdvargas17@utpl.edu.ec>
2024-10-26 12:56:14 -0500

    commit35

--- ProyectoTaller3/src/Ejercicio3.java
+++ ProyectoTaller3/src/Ejercicio3.java
@@ -1,3 +1,14 @@
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+    int a1, a2, a3, a4, a5, a6;
+    double respuest;
+    a1= 10;
+    a2= 2;
+    a3= 16;
+    a4= 20;
+    a5= 1;
+    a6= 11;
+    respuest = (Math.pow (a1, a2)) + (Math.sqrt(a3)) + (a4 / (a5 + a6));
+    System.out.println("respuest = " + respuest);
+    }
 }
--- ProyectoTaller3/src/Ejercicio5.java
+++ ProyectoTaller3/src/Ejercicio5.java
@@ -1,3 +1,13 @@
 public class Ejercicio5 {
-    
+    public static void main(String[] args) {
+        int a1, a2, a3, a4, a5;
+        double respuest;
+        a1= 2;
+        a2= 2;
+        a3= 12;
+        a4= 1;
+        a5= 3;
+        respuest= (Math.pow(a1, a2)+ a3-a4) / a5;
+        System.out.println("respuest = " + respuest);
+    }
 }
